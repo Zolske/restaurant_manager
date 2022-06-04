c22bd134dd5c8b1d85583866cc56eb425de84702
ZolsKe <zoltan.the.kepes@gmail.com>
2022-06-04 07:03:38 +0100

    comment out code

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,11 @@ from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 
 
+# use Django’s generic CreateView
 class SignUpView(CreateView):
+    # tell Django to use our CustomUserCreationForm
     form_class = CustomUserCreationForm
+    # redirect to login once a user signs up successfully
     success_url = reverse_lazy('login')
+    # template is named signup.html
     template_name = "registration/signup.html"
