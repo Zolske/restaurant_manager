a5fbefb9ed6d40d262947e0ae6c2719e1a85af2a
ZolsKe <zoltan.the.kepes@gmail.com>
2022-06-04 12:11:04 +0100

    add: Django debug toolbar

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,7 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('__debug__/', include('debug_toolbar.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("pages.urls")),
