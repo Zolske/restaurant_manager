9f67702ffad5d93622fe8b728f7f333b61cc193d
ZolsKe <zoltan.the.kepes@gmail.com>
2022-06-04 07:33:19 +0100

    test: pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+# SimpleTestCase is enough because homepage does not rely on the database
+from django.test import SimpleTestCase
+# import reverse to test URL and views
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location_homepageview(self):
+        response = self.client.get("/")
+        # homepage URL returns a 200 status code, no slug
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_view(self):
+        response = self.client.get(reverse("home"))
+        # homepage URL returns a 200 status code, slog 'home'
+        self.assertEqual(response.status_code, 200)
+        # use home.html
+        self.assertTemplateUsed(response, "home.html")
+        # contains “Home” in the response
+        self.assertContains(response, "Home")
